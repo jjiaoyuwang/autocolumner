897f52a226baf1617732199ca734039378ba0542
¨Joseph <joseph_wang@live.ie>
2022-05-22 19:12:21 +1000

    tested settings.py import and it works

--- sample/test_config.py
+++ sample/test_config.py
@@ -1,45 +1,28 @@
-import configparser
-parser = configparser.ConfigParser()
-parser.read('../sequence_params/sample_params.txt')
-# for sect in parser.sections():
-#     print('Section:', sect)
-    # for k,v in parser.items(sect):
-    #     print(' {} = {}'.format(k,v))
-    # print()
+import settings
 
-SERVO1_PIN = parser["Settings"]["SERVO1_PIN".lower()]
-SERVO2_PIN = parser["Settings"]["SERVO2_PIN".lower()]
-PUMP1_PIN = parser["Settings"]["PUMP1_PIN".lower()]
-PUMP2_PIN = parser["Settings"]["PUMP2_PIN".lower()]
-STEPPER1_PINS = parser["Settings"]["STEPPER1_PINS".lower()].split()
+# in theory, I can also run from settings import * but this could lead to poor readability, especially for someone not familiar with the code.
 
-print(type(STEPPER1_PINS))
-print(STEPPER1_PINS)
-# SERVO1_PIN = 5 # pins for servo1 (tap)
-# SERVO2_PIN = 6 # pins for servo2 (sep funnel)
-# PUMP1_PIN = 2 # pins for pump1 (hexanes)
-# PUMP2_PIN = 3 # pins for pump2 (polar component)
-# STEPPER1_PINS = [7, 8, 9] # pins for stepper1 (theta1, closest to origin)
-# STEPPER2_PINS = [10, 11, 12, 13] # pins for stepper2 (second axis)
+# general settings
+SERVO1_PIN = settings.SERVO1_PIN # pins for servo1 (tap)
+SERVO2_PIN = settings.SERVO2_PIN # pins for servo2 (sep funnel)
+PUMP1_PIN = settings.PUMP1_PIN # pins for pump1 (hexanes)
+PUMP2_PIN = settings.PUMP2_PIN # pins for pump2 (polar component)
+STEPPER1_PINS = settings.STEPPER1_PINS # pins for stepper1 (theta1, closest to origin)
+STEPPER2_PINS = settings.STEPPER2_PINS
 
-# [Servo]
-# # servo specific variables
-# # 125. degrees (open) means vacuum open. 25 degrees means vacuum closed (i.e. open to atmosphere)
-# # 0 degrees is open, 90 degrees stopcock is closed
-# SERVO1_OPEN_CLOSE_ANGLES = [125., 25.] 
-# SERVO2_OPEN_CLOSE_ANGLES = [0., 90.] 
+# servo specific variables
+SERVO1_OPEN_CLOSE_ANGLES = settings.SERVO1_OPEN_CLOSE_ANGLES # 125. degrees (open) means vacuum open. 25 degrees means vacuum closed (i.e. open to atmosphere)
+SERVO2_OPEN_CLOSE_ANGLES = settings.SERVO2_OPEN_CLOSE_ANGLES # 0 degrees is open, 90 degrees stopcock is closed
 
-# [Pump]
-# # pump specific variables
-# PUMP1_FLOW_RATE = 20. # set to 20 mL / min for now as a test
-# PUMP2_FLOW_RATE = 20.
+# pump specific variables
+PUMP1_FLOW_RATE = settings.PUMP1_FLOW_RATE # set to 20 mL / min for now as a test
+PUMP2_FLOW_RATE = settings.PUMP2_FLOW_RATE 
 
-# [Robotic Arm]
-# # robotic arm specific variables
-# FRACTION_COORDS_PATH = "../scripts/test_coords_20052022.pickle" # path to numpy array of coordinates
-# ARM1_LENGTH = 86.
-# ARM2_LENGTH = 55.
-# MOTOR1_TYPE = 'NEMA17'
-# MOTOR1_REVSTEPS = 3200
-# MOTOR2_TYPE = '28BYJ-48'
-# MOTOR2_REVSTEPS = 2038
+# robotic arm specific variables
+FRACTION_COORDS_PATH = settings.FRACTION_COORDS_PATH # path to numpy array of coordinates
+ARM1_LENGTH = settings.ARM1_LENGTH
+ARM2_LENGTH = settings.ARM2_LENGTH
+MOTOR1_TYPE = settings.MOTOR1_TYPE
+MOTOR1_REVSTEPS = settings.MOTOR1_REVSTEPS
+MOTOR2_TYPE = settings.MOTOR2_TYPE 
+MOTOR2_REVSTEPS = settings.MOTOR2_REVSTEPS 
